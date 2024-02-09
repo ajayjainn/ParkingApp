a7470ea69b7411b62dbc7365d95a3b4d1a7f3361
Ajay Jain <ajayjain1024@gmail.com>
2024-02-10 04:23:26 +0530

    Add Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
+import { Input } from './components/ui/input'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
 
   return (
     <> 
-      
+    <Input/>
+      <Button>Hello world</Button>
     </>
   )
 }
