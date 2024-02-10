8e9b0954c934ab819e98309d7ce40577b45b2245
JeetJani-11 <121782735+JeetJani-11@users.noreply.github.com>
2024-02-10 15:53:37 +0530

    Delete client directory

--- server/client/src/features/users/usersApiSlice.js
+++
@@ -1,8 +0,0 @@
-import { baseApiSlice } from "../api/baseApiSlice";
-
-export const usersApiSlice = baseApiSlice.injectEndpoints({
-  endpoints: (build)=>({
-  }),
-})
-
-export const {useGetUserProfileQuery} = usersApiSlice
